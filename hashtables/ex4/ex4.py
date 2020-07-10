33bcc8bfe8f128d4c586b26ff5aa8488ccd0c079
Nathan <nateyl.b@gmail.com>
2020-07-10 10:34:40 -0700

    ex4 passed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    pos = {}
+    neg = {}
+    result = []
+    for num in a:
+        if num > 0:
+            pos[num] = True
+        else:
+            neg[num] = True
+    for key in pos:
+        if(neg.get(key*-1)):
+            result.append(key)
+
 
     return result
 
